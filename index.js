06b7b23a53c4af8a74a4e0b5bd1e4430d8087e6f
Balanceact <makildur@hotmail.com>
2024-07-03 16:28:53 +0200

    Added While loop for operator and fixed the bug with + operator by ingeger conversion.

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 alert("Welcome! You'll be asked to input two numbers and an operator.");
-const first = prompt("Please input the first number.");
-const operator = prompt("Please input an operator (+, -, *, /).");
-const second = prompt("Please input the second number.");
 let result = 0;
-switch (operator) {
-    case "+":
-        result = first + second;
-        break;
-    case "-":
-        result = first - second;
-        break;
-    case "*":
-        result = first * second;
-        break;
-    case "/":
-        result = first / second;
-        break;
-    default:
-        alert("Please input a valid operator.");
-        break;
+let loop = true;
+const first = prompt("Please input the first number.");
+const second = prompt("Please input the second number.");
+let operator = " ";
+while (loop) {
+    operator = prompt("Please input an operator (+, -, *, /).");
+    switch (operator) {
+        case "+":
+            result = parseInt(first) + parseInt(second);
+            loop = false;
+            break;
+        case "-":
+            result = first - second;
+            loop = false;
+            break;
+        case "*":
+            result = first * second;
+            loop = false;
+            break;
+        case "/":
+            result = first / second;
+            loop = false;
+            break;
+        default:
+            alert("Please input a valid operator.");
+            break;
+    }
 }
 
 alert(first + " " + operator + " " + second + " = " + result);
 alert("Have a nice day! :)");
+
+
+function add(a, b) {
+    return a + b;
+}
